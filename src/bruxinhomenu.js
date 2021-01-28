d9a3d5a897d0c54a9887dbc9a65d67d60aa29d03
Kaukakakak <76785280+Kaukakakak@users.noreply.github.com>
2021-01-28 09:39:13 -0300

    Add files via upload

---
+++ src/bruxinhomenu.js
@@ -0,0 +1,12 @@
+const bruxinhomenu = (prefix, pushname) => {
+    return `◪ *Comandos do S4ANVIC*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.bruxinhomenu = bruxinho
